d415f83b95a977a37de0899dd6e2c70686e50e1c
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2021-11-05 20:52:20 +0530

    Update __main__.py

--- GabiBraunRobot/__main__.py
+++ GabiBraunRobot/__main__.py
@@ -213,7 +213,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "A jujutsu sorcerer never sleeps!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+            "Hey how ya doin!\n<b>Haven't slept since:</b> <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
